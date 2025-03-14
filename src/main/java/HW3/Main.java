2c900c17e3bcc645306afaef551145226e4c4e06
AleksandrBobrovskikh <bobrosan@gmail.com>
2025-03-15 01:53:01 +0300

    Начал работать с ArrayList

--- src/main/java/HW3/Main.java
+++ src/main/java/HW3/Main.java
@@ -1,4 +1,10 @@
 package HW3;
 
 public class Main {
-}
+    public static void main(String[] args) {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        list.add(1);
+        list.print();
+    }
+    }
+
