67bcf90c43ca1220bb610b2bdd515ecd5ed57bdb
AleksandrBobrovskikh <bobrosan@gmail.com>
2025-03-17 14:40:39 +0300

    Начал работать с MyLinkedList

--- src/main/java/HW3/Main.java
+++ src/main/java/HW3/Main.java
@@ -2,13 +2,7 @@ package HW3;
 
 public class Main {
     public static void main(String[] args) {
-        MyArrayList<Integer> list = new MyArrayList<>();
-        list.add(1);
-        list.print();
-        list.add(1);
-        list.print();
-        list.add(1);
-        list.print();
+
     }
     }
 
